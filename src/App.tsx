be334094720ca0026f803d39345dc0383f0a5c34
funayamateppei <t.funayama0217@gmail.com>
2024-03-10 23:57:40 +0900

    MediaStream取得処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import logo from "./logo.svg";
 import "./App.css";
+import * as MUI from "@mui/material";
+import { useState, useRef } from "react";
 
 function App() {
+    const [videoStream, setVideoStream] = useState<MediaStream>();
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const handleStart = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            setVideoStream(stream);
+            if (videoRef.current) {
+                videoRef.current.srcObject = stream;
+            }
+        } catch (error) {
+            console.error("Error accessing video stream: ", error);
+        }
+    };
+
+    const handleStop = () => {
+        if (videoStream) {
+            videoStream.getTracks().forEach((track) => track.stop());
+            setVideoStream(undefined);
+            if (videoRef.current) {
+                videoRef.current.srcObject = null;
+            }
+        }
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <video ref={videoRef} autoPlay></video>
+
+                <MUI.Stack direction="row" gap={1}>
+                    <MUI.Button variant="contained" onClick={handleStart}>
+                        Start
+                    </MUI.Button>
+                    <MUI.Button variant="contained" onClick={handleStop}>
+                        Stop
+                    </MUI.Button>
+                </MUI.Stack>
             </header>
         </div>
     );
